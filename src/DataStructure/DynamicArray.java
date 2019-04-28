ae8049e491264388491e4d8a7e4bab4e230589df
EA Rashel <rashel320@gmail.com>
2019-04-29 00:17:03 +0600

    created a dynamic array class

--- src/DataStructure/DynamicArray.java
+++ src/DataStructure/DynamicArray.java
@@ -1,6 +1,116 @@
 package DataStructure;
 
-public class DynamicArray {
+import java.util.Iterator;
+
+public class DynamicArray<T> implements Iterable<T> {
 
 
+    private T[] arr;
+    private int len = 0; // length user thinks array is
+    private int capacity = 0; // actual array size
+
+
+    public DynamicArray() {
+        this(16);
+    }
+
+    public DynamicArray(int capacity) {
+        if (capacity < 0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+
+    public int size() {
+        return len;
+    }
+
+    ;
+
+    boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T get(int index) {
+        return arr[index];
+    }
+
+    public void set(int index, T elem) {
+        arr[index] = elem;
+    }
+
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = null;
+        }
+        len = 0;
+    }
+
+    public void add(T elem) {
+
+        if (len + 1 >= capacity) {
+            if (capacity == 0) capacity = 1;
+            else capacity *= 2;
+
+            T[] new_arr = (T[]) new Object[capacity];
+            for (int i = 0; i < len; i++) {
+                new_arr[i] = arr[i];
+            }
+
+            arr = new_arr;
+        }
+
+        arr[len++] = elem;
+
+
+    }
+
+
+    public T removeAt(int rm_index) {
+
+        if (rm_index >= len && rm_index < 0) throw new IndexOutOfBoundsException();
+
+        T data = arr[rm_index];
+        T[] new_arr = (T[]) new Object[len - 1];
+
+        for (int i = 0, j = 0; i < len; i++, j++) {
+            if (i == rm_index) j--;
+            else new_arr[j] = arr[i];
+        }
+        arr = new_arr;
+        capacity = --len;
+        return data;
+    }
+
+    public boolean remove(Object obj) {
+
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj)) {
+                removeAt(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public int indexOf(Object obj) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i].equals(obj))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public boolean contains(Object obj) {
+        return indexOf(obj) != -1;
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,10 @@
 
-import CustomIterable.CustomArrayList;
-import CustomIterable.NormalArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        CustomArrayList customArrayList = new CustomArrayList();
 
-        customArrayList.add("Test 1");
-        customArrayList.add("Test 2");
-        customArrayList.add("Test 3");
-
-
-        NormalArrayList normalArrayList = new NormalArrayList();
-
-        normalArrayList.add("Normal 1");
-        normalArrayList.add("Normal 2");
-        normalArrayList.add("Normal 3");
-
-
-        // this giving us error bcoz this calss is not iterable
-//        for (String normalVal: normalArrayList) {
-//            System.out.println(normalVal);
-//        }
-
-
-        // this is iterable as the class customarrlist is iterable
-        for(Object customVal : customArrayList) {
-            System.out.println(customVal);
-        }
     }
 }
