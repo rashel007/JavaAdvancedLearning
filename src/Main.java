d694eac86aa830930e4d0557b6e6d62e31743f0f
EA Rashel <rashel320@gmail.com>
2019-05-01 20:21:07 +0600

    check implementation of dynamicArray class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-
-
+import DataStructure.DynamicArray;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        DynamicArray<String> array = new DynamicArray<>();
+
+        array.add("Name 1");
+        array.add("Name 2");
+        array.add("Name 3");
+
+        System.out.println(array.size());
+
+        array.removeAt(1);
+        System.out.println(array.size());
 
     }
 }
